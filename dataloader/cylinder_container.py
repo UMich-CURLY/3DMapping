0be871d4d705c977257efb485cf49fcad017ef5e
Arthur Zhang <arthurzh@umich.edu>
2021-11-17 17:41:09 -0500

    Implemented and tested correctness of cylindrical coordinate cell assignment

--- dataloader/cylinder_container.py
+++ dataloader/cylinder_container.py
@@ -1,4 +1,5 @@
 import os
+import pdb
 import numpy as np
 import yaml
 
@@ -14,12 +15,14 @@ import yaml
 """
 class CylinderContainer:
     def __init__(self, grid_size,
-        max_bound=np.array([2*np.pi, 50, np.pi/2.0], dtype=np.float32),
-        min_bound=np.array([0, 0, -np.pi/2.0], dtype=np.float32)):
+        min_bound=np.array([0, -2.0*np.pi, 0], dtype=np.float32),
+        max_bound=np.array([20, 2.0*np.pi, 10], dtype=np.float32)):
         """
         Constructor that creates the cylinder coordinate container
 
         :param grid_size: The number of cells that we want to divide the grid into
+        :param max_bound: [max radial distance, max_azimuth, max_height]
+        :param min_bound: [min radial distance, min_azimuth, min_height]
 
         """
         self.grid_size = grid_size
@@ -61,6 +64,7 @@ class CylinderContainer:
         Returns the voxel centroid that the cartesian coordinate falls in
 
         :param xyz: nx3 np array where rows are points and cols are x,y,z
+        :return: nx1 np array where rows are points and col is value at each point
         """
         return self.voxels[self.grid_ind(input_xyz)]
 
@@ -73,10 +77,17 @@ class CylinderContainer:
         self.voxels[self.grid_ind(input_xyz)] = input_value
 
     def grid_ind(self, input_xyz):
+        """
+        Returns index of each cartesian coordinate in grid
+
+        :param input_xyz: nx3 np array where rows are points and cols are x,y,z
+        :return: nx3 array where rows are points and cols are indices for that point
+        """
         xyz_pol = self.cart2polar(input_xyz)
 
         grid_ind = (np.floor((np.clip(xyz_pol, self.min_bound, self.max_bound) 
                     - self.min_bound) / self.intervals)).astype(np.int)
+
         return grid_ind
 
     def get_voxel_centers(self, input_xyz):
@@ -84,29 +95,39 @@ class CylinderContainer:
         Return voxel centers corresponding to each input xyz cartesian coordinate
 
         :param input_xyz: nx3 np array where rows are points and cols are x,y,z
+
+        :return:  nx3 np array where rows are points and cols are x, y, z
         """
-        input_xyz_size = np.shape(input_xyz)[0]
-        voxel_position = np.zeros(input_xyz_size, dtype=np.float32)
-        dim_array = np.ones(len(input_xyz_size) + 1, int)
-        dim_array[0] = -1
-        voxel_position = np.indices(input_xyz_size) * self.intervals.reshape(dim_array) \
-                            + self.min_bound.reshape(dim_array)
-        voxel_position = self.polar2cat(voxel_position)
         
         # Center data on each voxel centroid for cylindrical coordinate PTnet
         voxel_centers = (self.grid_ind(input_xyz).astype(np.float32) + 0.5) * \
                         self.intervals + self.min_bound
-        return voxel_centers
+        return self.polar2cart(voxel_centers)
 
     def cart2polar(self, input_xyz):
+        """
+        Converts cartesian to polar coordinates
+
+        :param input_xyz_polar:  nx3 np array where rows are points and cols are x,y,z
+
+        :return: nx3 np array where rows are points and cols are r,theta,z
+        """
         rho = np.sqrt(input_xyz[:, 0] ** 2 + input_xyz[:, 1] ** 2)
         phi = np.arctan2(input_xyz[:, 1], input_xyz[:, 0])
         return np.stack((rho, phi, input_xyz[:, 2]), axis=1)
 
 
-    def polar2cat(self, input_xyz_polar):
-        x = input_xyz_polar[0] * np.cos(input_xyz_polar[1])
-        y = input_xyz_polar[0] * np.sin(input_xyz_polar[1])
-        return np.stack((x, y, input_xyz_polar[2]), axis=0)
+    def polar2cart(self, input_xyz_polar):
+        """
+        Converts polar to cartesian coordinates
+
+        :param input_xyz_polar: nx3 np array where rows are points and cols are r,theta,z
+
+        :return: nx3 np array where rows are points and cols are x,y,z
+        """
+        x = input_xyz_polar[:, 0] * np.cos(input_xyz_polar[:, 1])
+        y = input_xyz_polar[:, 0] * np.sin(input_xyz_polar[:, 1])
+
+        return np.stack((x, y, input_xyz_polar[:, 2]), axis=1)
 
 
--- dataloader/dataloader_main.py
+++ dataloader/dataloader_main.py
@@ -1,20 +1,68 @@
 import os
 import pdb
 import time
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy.random.mtrand import rand
+from mpl_toolkits.mplot3d import Axes3D
 
 from cylinder_container import CylinderContainer
 
 """
 Driver file for loading data into cylindrical coordinate voxels
 """
+
+def load_dummy_point_cloud(min_bound, max_bound, num_points):
+    rand_radius = np.random.uniform(    min_bound[0], max_bound[0], 
+                                        num_points).reshape((num_points, 1))
+    rand_azimuth = np.random.uniform(   min_bound[1], max_bound[1], 
+                                        num_points).reshape((num_points, 1))
+    rand_height = np.random.uniform(    min_bound[2], max_bound[2], 
+                                        num_points).reshape((num_points, 1))
+
+    random_points = np.hstack((rand_radius, rand_azimuth, rand_height))
+
+    return random_points
+
+def plot_points(points, fig=None, marker='o', color=(1, 0, 0)):
+    ax = []
+    if fig is None:
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+    else:
+        ax = fig.gca()
+
+    ax.scatter3D(points[:,0], points[:,1], points[:,2], marker='o', color=color)
+    # ax.scatter(points[:,0], points[:,1], marker='o', color=color)
+    return fig
+
 def main():
     # TODO: Run simulation here
-    while(1):
-        # Initializes cylinder container with 1000 cells and default range settings
-        # 
-        # Note: Not making container size scale with the number of
-        #       occupied cells because we want to ray trace in empty cells too
-        cells = CylinderContainer(1000)
+
+    # Initializes cylinder container with 1000 cells and default range settings
+    # 
+    # Note: Not making container size scale with the number of
+    #       occupied cells because we want to ray trace in empty cells too
+    num_points = 1000
+    num_cells = 20
+
+    min_bound = np.array([0, -2.0*np.pi, 0])
+    max_bound = np.array([20, 2.0*np.pi, 20])
+
+    cells = CylinderContainer(num_cells, min_bound, max_bound)
+
+    random_points = cells.polar2cart(load_dummy_point_cloud(min_bound, max_bound, num_points))
+    
+    fig = plot_points(random_points)
+
+    # Visualize points mapped to centroid
+    voxel_centers = cells.get_voxel_centers(random_points)
+
+    fig = plot_points(voxel_centers, fig, 'x', (0, 0, 1))
+    plt.show()
+
+
 
 if __name__ == '__main__':
     main()
